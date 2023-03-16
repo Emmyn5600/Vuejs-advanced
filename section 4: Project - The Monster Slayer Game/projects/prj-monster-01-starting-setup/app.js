c924125406cc779880cb535e108ceb21d5e2a2ce
emmyn5600 <emmy66418@gmail.com>
2023-03-16 10:10:36 +0200

    Implement heal method

--- section 4: Project - The Monster Slayer Game/projects/prj-monster-01-starting-setup/app.js
+++ section 4: Project - The Monster Slayer Game/projects/prj-monster-01-starting-setup/app.js
@@ -1,4 +1,4 @@
-function getRandomValue(min, max){
+function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
@@ -7,42 +7,53 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      currentRound: 0
+      currentRound: 0,
     };
   },
 
-  computed:{
-    monsterBarStyles(){
-      return { width: this.monsterHealth + '%' }
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
     },
 
-    playerBarStyles(){
-      return { width: this.playerHealth + '%' }
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
     },
 
-    mayUseSpecialAttack(){
-      return this.currentRound % 3 !== 0 
-    }
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
 
   methods: {
     attackMonster() {
       this.currentRound++;
-      const attackValue = getRandomValue(5, 12)
+      const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
     attackPlayer() {
-      const attackValue = getRandomValue(8, 15)
+      const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
     },
 
-    specialAttackMonster(){
-      this.currentRound++
-      const attackValue = getRandomValue(10, 25)
-      this.monsterHealth -= attackValue
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
       this.attackPlayer();
-    }
+    },
+
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
