c59b08e15f85df36f6283b257567547443b8224a
emmyn5600 <emmy66418@gmail.com>
2023-03-22 12:23:24 +0200

    Implement the emit custom events child to parent communication

--- section 8: Component Communication/cmp-communication-01-starting-setup/src/App.vue
+++ section 8: Component Communication/cmp-communication-01-starting-setup/src/App.vue
@@ -7,10 +7,12 @@
       <friend-contact
         v-for="friend in friends"
         :key="friend.id"
+        :id="friend.id"
         :name="friend.name"
         :phone-number="friend.phone"
         :email-address="friend.email"
-        :is-favorite = "true"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
       ></friend-contact>
     </ul>
   </section>
@@ -35,6 +37,14 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const friendIdentifier = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      friendIdentifier.isFavorite = !friendIdentifier.isFavorite;
+    },
   },
 };
 </script>
