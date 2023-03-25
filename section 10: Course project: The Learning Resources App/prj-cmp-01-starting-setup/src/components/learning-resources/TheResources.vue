bd8f62ba174c2cf0d5f09a3c96a35518e6e342e2
emmyn5600 <emmy66418@gmail.com>
2023-03-25 23:55:54 +0200

    Fetching user input

--- section 10: Course project: The Learning Resources App/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ section 10: Course project: The Learning Resources App/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -14,7 +14,9 @@
         Add Resource</base-button
       >
     </base-card>
-    <component :is="selectedTab"></component>
+    <keep-alive>
+      <component :is="selectedTab"></component>
+    </keep-alive>
   </div>
 </template>
 
@@ -49,6 +51,7 @@ export default {
   provide() {
     return {
       resources: this.storeResources,
+      addResource: this.addResource,
     };
   },
   computed: {
@@ -62,6 +65,16 @@ export default {
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storeResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     },
   },
 };
