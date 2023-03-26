2ba2a89842c671f42913a0aca90b73cc1e89e958
emmyn5600 <emmy66418@gmail.com>
2023-03-26 21:18:30 +0200

    V-model on custom components

--- section 11: Forms/forms-01-starting-setup/src/components/TheForm.vue
+++ section 11: Forms/forms-01-starting-setup/src/components/TheForm.vue
@@ -99,7 +99,7 @@
       </div>
     </div>
     <div class="form-control">
-      <rating-control></rating-control>
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div class="form-control">
       <input
@@ -130,6 +130,7 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
+      rating: null,
       confirm: false,
       userNameValidity: 'pending',
     };
@@ -153,6 +154,9 @@ export default {
       console.log('Confirm?');
       console.log(this.confirm);
       this.confirm = false;
+      console.log('Rating');
+      console.log(this.rating);
+      this.rating = null;
     },
     validateInput() {
       if (this.userName === '') {
