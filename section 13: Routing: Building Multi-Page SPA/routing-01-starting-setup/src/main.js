792141c0bfdf6050780a6ee6cf58709cd430b24b
emmyn5600 <emmy66418@gmail.com>
2023-03-28 11:29:54 +0200

    Redirect and catch all routes

--- section 13: Routing: Building Multi-Page SPA/routing-01-starting-setup/src/main.js
+++ section 13: Routing: Building Multi-Page SPA/routing-01-starting-setup/src/main.js
@@ -5,16 +5,20 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UserList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    // {path: '/', redirect: '/teams'},
     {
       path: '/teams',
       component: TeamsList,
+      alias: '/',
     },
     { path: '/users', component: UserList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
