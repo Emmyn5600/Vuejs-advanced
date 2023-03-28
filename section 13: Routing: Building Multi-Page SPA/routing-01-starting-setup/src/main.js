49e21d31b7211bf1dc8544ca633100e3fcf58a85
emmyn5600 <emmy66418@gmail.com>
2023-03-28 20:37:18 +0200

    Introduction navigation guards

--- section 13: Routing: Building Multi-Page SPA/routing-01-starting-setup/src/main.js
+++ section 13: Routing: Building Multi-Page SPA/routing-01-starting-setup/src/main.js
@@ -32,14 +32,25 @@ const router = createRouter({
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
-  scrollBehavior(to, from, savePosition) {
-    console.log(to, from);
+  scrollBehavior(_, _2, savePosition) {
+    // console.log(to, from);
     if (savePosition) {
       return savePosition;
     }
     return { left: 0, top: 0 };
   },
 });
+
+router.beforeEach(function(to, from, next){
+    console.log('Global beforeEach');
+    console.log(to, from);
+    // if(to.name === 'team-members'){
+    //   next()
+    // }else{
+    //   next({name: 'team-members', params:{ teamId: 't2'}});
+    // }
+    next()
+})
 
 const app = createApp(App);
 
