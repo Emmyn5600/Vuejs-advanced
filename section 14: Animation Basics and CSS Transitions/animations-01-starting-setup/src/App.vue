f7eb032d39ed6c67f2e520f4edc30ba60f59066c
emmyn5600 <emmy66418@gmail.com>
2023-03-29 10:41:11 +0200

    using a custom css class name

--- section 14: Animation Basics and CSS Transitions/animations-01-starting-setup/src/App.vue
+++ section 14: Animation Basics and CSS Transitions/animations-01-starting-setup/src/App.vue
@@ -4,7 +4,8 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <!-- <transition name="para" enter-to-class="some-class" enter-active-class=""> -->
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -29,7 +30,7 @@ export default {
   },
   methods: {
     animateBlock() {
-      this.animatedBlock = true;
+      this.animatedBlock = !this.animatedBlock;
     },
     showDialog() {
       this.dialogIsVisible = true;
@@ -92,32 +93,32 @@ button:active {
   animation: slide-scale 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+.para-enter-from {
   /* opacity: 0;
   transform: translateY(-30px); */
 }
 
-.v-enter-active {
+.para-enter-active {
   /* transition: all 0.3s ease-out; */
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   /* opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-from {
+.para-leave-from {
   /* opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-active {
+.para-leave-active {
   /* transition: all 0.3s ease-out; */
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-leave-to {
+.para-leave-to {
   /* opacity: 0;
   transform: translateY(30px); */
 }
