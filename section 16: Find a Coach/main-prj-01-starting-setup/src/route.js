7069731cac0fcaae4e772a5c450c531e8383f8dc
emmyn5600 <emmy66418@gmail.com>
2023-03-31 19:27:38 +0200

    working on the main layout and styling

--- section 16: Find a Coach/main-prj-01-starting-setup/src/route.js
+++ section 16: Find a Coach/main-prj-01-starting-setup/src/route.js
@@ -5,12 +5,12 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistation from './pages/coaches/CoachRegister.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import NotFound from './pages/NoFound.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect0: '/coaches' },
+    { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
