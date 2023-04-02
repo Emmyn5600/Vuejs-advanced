0d6863402538b45497b1654d61da80cf306ccc1c
emmyn5600 <emmy66418@gmail.com>
2023-04-02 09:18:42 +0200

    Adding vuex and coach data

--- section 16: Find a Coach/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ section 16: Find a Coach/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,8 +5,25 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      LIST OF THE COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.lastName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+//use computed to get getters
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches']; //first is the namespace name and second getter name
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
