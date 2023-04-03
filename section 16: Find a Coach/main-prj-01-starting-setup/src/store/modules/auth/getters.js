a8a68d768840b15a2a4f3cac81e871eb7f80e922
emmyn5600 <emmy66418@gmail.com>
2023-04-03 16:46:25 +0200

    adding auto logout

--- section 16: Find a Coach/main-prj-01-starting-setup/src/store/modules/auth/getters.js
+++ section 16: Find a Coach/main-prj-01-starting-setup/src/store/modules/auth/getters.js
@@ -5,7 +5,10 @@ export default {
   token(state) {
     return state.token;
   },
-  isAuthenticated(state){
-    return !!state.token
-  }
+  isAuthenticated(state) {
+    return !!state.token;
+  },
+  didAutoLogout(state) {
+    return state.didAutoLogout;
+  },
 };
