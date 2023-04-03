f181e31b777ece3bf6fe99ca1453e836ca1e3465
emmyn5600 <emmy66418@gmail.com>
2023-04-03 14:07:29 +0200

    attaching the token to outgoing requests

--- section 16: Find a Coach/main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ section 16: Find a Coach/main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -30,8 +30,10 @@ export default {
 
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://find-a-coach-6427a-default-rtdb.firebaseio.com/requests/${coachId}.json`
+      `https://find-a-coach-6427a-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
     if (!response.ok) {
